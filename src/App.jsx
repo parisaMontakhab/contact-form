b1687b2a66e89b7dd61b2cf0879c43c28dfe1bc1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-03 13:12:34 +0200

    feat: add form tag with inline style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ export default function App() {
   return (
    
     <div className="min-h-screen flex items-center justify-center">
-      
+      <form className="bg-neutral_white rounded-lg w-full max-w-2xl">
+        
+
+      </form>
 
     </div>
   );
