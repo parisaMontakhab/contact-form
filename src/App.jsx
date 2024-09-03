9fff758031ca39a64a7bea81c25a34331a8fe7ae
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-03 13:24:47 +0200

    refactor: changed style of h2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     <div className="min-h-screen flex items-center justify-center">
       <form className="bg-neutral_white rounded-xl w-full max-w-2xl p-8">
         
-        <h2 className="text-neutral_graydark text-2xl font-sans font-bold mb-5">Contact Us</h2>
+        <h2 className="text-neutral_graydark text-3xl font-sans font-bold mb-5">Contact Us</h2>
 
       </form>
 
