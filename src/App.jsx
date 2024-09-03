e2255f081cc3086dc1c7d90042d60ebbc099feb1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-03 13:22:45 +0200

    feat: add h2 tag with inline style

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ export default function App() {
   return (
    
     <div className="min-h-screen flex items-center justify-center">
-      <form className="bg-neutral_white rounded-lg w-full max-w-2xl">
+      <form className="bg-neutral_white rounded-xl w-full max-w-2xl p-8">
         
+        <h2 className="text-neutral_graydark text-2xl font-sans font-bold mb-5">Contact Us</h2>
 
       </form>
 
