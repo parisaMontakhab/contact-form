e01dd9118a59e4dc85c7958c1eaa5b14741fdcf7
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-04 19:48:21 +0200

    refactor: customized star-form

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ export default function App() {
 
           <div className="w-1/2">
           <label className="label-form" for='firstname'>First Name
-            <span className=" text-primary_greenmedium font-sans text-lg mx-1">*</span>
+            <span className="star-form">*</span>
           </label>
           <input className="w-full px-3 py-2 border border-neutral_graymedium rounded-lg" type="text" id="firstname"/>
           </div>
 
           <div className="w-1/2">
           <label className="label-form" for='lastname'>Last Name
-            <span className=" text-primary_greenmedium font-sans text-lg mx-1">*</span>
+            <span className="star-form">*</span>
           </label>
           <input className="w-full px-3 py-2 border border-neutral_graymedium rounded-lg" type="text" id="lastname"/>
           </div>
@@ -39,7 +39,7 @@ export default function App() {
           <div className="w-1/2 relative">
          
           <label className="label-form" for='querytype'>Query Type
-            <span className=" text-primary_greenmedium font-sans text-lg mx-1">*</span>
+            <span className="star-form">*</span>
           </label>
           <input className="w-full px-3 py-2 border border-neutral_graymedium rounded-lg "  id="querytype"/>
           <input type="radio" id='general' className="absolute bottom-4 left-6 "/>
@@ -58,7 +58,7 @@ export default function App() {
 
         <div className="mb-4">
       <label className="label-form" for="message">Message
-      <span className=" text-primary_greenmedium font-sans text-lg mx-1">*</span>
+      <span className="star-form">*</span>
       </label>
       <textarea className="w-full px-3 py-2 border border-neutral_graymedium rounded-lg" id="message" ></textarea>
     </div>
@@ -67,7 +67,7 @@ export default function App() {
 
           <input type="checkbox" className=""/>
           <span className="ml-3  text-neutral_graydark font-sans text-base ">I consent to being contaced by the team</span>
-          <span className=" text-primary_greenmedium font-sans text-lg mx-1 ">*</span>
+          <span className="star-form">*</span>
 
         </div>
 
