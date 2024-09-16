fc4dfac2ce4467169cac41f0256f5e3aa08b7739
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-16 13:27:29 +0200

    refactor: changed the config about tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,11 @@ module.exports = {
     extend: {
       colors :{
         primary_greenlight :'hsl(148, 38%, 91%)',
-        primary_greenmedium : 'hsl(169, 82%, 27%)',
-        primary_red : 'hsl(0, 66%, 54%)',
-        neutral_white : 'hsl(0, 0%, 100%)',
-        neutral_graymedium : 'hsl(186, 15%, 59%)',
-        neutral_graydark : 'hsl(187, 24%, 22%)'
+      
         
       },
-      fontFamily: {
-        sans: ['Karla', 'sans-serif'],
-      },
-      fontWeight: {
-        normal: 400,
-        bold: 700,
-      },
-      fontSize: {
-        'label': '16px',
-      },
+    
+      
     },
   },
   plugins: [],
