abb1064dfd2d1283abae5f75c48f07371c7720a0
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-24 11:37:57 +0200

    feat: add all of consts that we want check them

--- tests/components/MyForm.test.jsx
+++ tests/components/MyForm.test.jsx
@@ -7,6 +7,15 @@ import MyForm from '../../src/components/MyForm';
 describe('MyForm', () => {
     it('should be renders form elements correctly', () => {
         render(<MyForm/>)
+
+        const inputFirstName = screen.getByLabelText(/First Name/i);
+        const inputLastName = screen.getByLabelText(/Last Name/i);
+        const inputEmail = screen.getByLabelText(/Email Adress/i);
+        const inputGeneralquery = screen.getByLabelText(/General Enquiry/i);
+        const inputSupportQuery = screen.getByLabelText(/Support Request/i);
+        const textAreaElement = screen.getByLabelText(/Message/i);
+        const checkBoxElement = screen.getByRole('checkbox');
+        const submitButton = screen.getByRole('button',{name:/submit/i});
     })
     
 })
