435ed3e5d00c2216a2ddb983e17faf69173fa13f
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-25 21:37:39 +0200

    feat: add another test in MyForm.test.jsx : should accept valid first name and last name

--- tests/components/MyForm.test.jsx
+++ tests/components/MyForm.test.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { it, expect, describe } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import MyForm from '../../src/components/MyForm';
 
 
 describe('MyForm', () => {
+    beforeEach(() => {
+        render(<MyForm />); 
+      });
 
     it('should we have all of the elements when the form is rendered ', () => {
-        render(<MyForm/>)
-
+        
         expect(screen.getByText(/Contact Us/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/First Name/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/Last Name/i)).toBeInTheDocument();
@@ -19,6 +21,18 @@ describe('MyForm', () => {
         expect(screen.getByLabelText(/Message/i)).toBeInTheDocument();
         expect(screen.getByRole('checkbox')).toBeInTheDocument();
         expect(screen.getByRole('button',{name:/submit/i})).toBeInTheDocument();
-    })
+    });
+
+    it('should accept valid firstName and lastName', () => {
+        
+      fireEvent.change(screen.getByLabelText(/First Name/i), {target: {value: 'Parisa'}});
+      fireEvent.change(screen.getByLabelText(/Last Name/i), {target: {value: 'Montakhabisani'}});
+      fireEvent.click(screen.getByRole('button',{name:/submit/i}));
+
+      expect(screen.queryByText(/Invalid first name/i)).not.toBeInTheDocument();
+      expect(screen.queryByText(/Invalid last name/i)).not.toBeInTheDocument();
+        
+    });
+
     
 })
