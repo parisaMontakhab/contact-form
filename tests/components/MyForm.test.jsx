14360d08471b008b9baa36a60c384f4110a06c59
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-09-30 11:48:04 +0200

    test--feat: should return invalid name if first name and last name are not between 2 and 50 charecters

--- tests/components/MyForm.test.jsx
+++ tests/components/MyForm.test.jsx
@@ -22,7 +22,7 @@ describe("MyForm", () => {
     expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
   });
 
-  it("should not accept if value of inputs are missing, like first name,last name,email,message", async () => {
+  it("should return this field is required if value of inputs are missing, like first name,last name,email,message", async () => {
     const user = userEvent.setup();
 
     await user.clear(screen.getByLabelText(/First Name/i));
@@ -34,11 +34,32 @@ describe("MyForm", () => {
     expect(screen.queryAllByText(/This field is required/i)).toHaveLength(4);
   });
 
-  it("should not accept invalid first name and last name with number and like @/&..", async () => {
+  it("should return invalid first name and last name with number and like @/&..", async () => {
     const user = userEvent.setup();
 
     await user.type(screen.getByLabelText(/First Name/i), "Pari@123");
     await user.type(screen.getByLabelText(/Last Name/i), "Monta$$123");
+    await user.click(screen.getByRole("button", { name: /submit/i }));
+
+    expect(screen.queryByText(/Invalid first name/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Invalid last name/i)).toBeInTheDocument();
+  });
+  it("should return invalid name if first name and last name are not between 2 and 50 charecters ", async () => {
+    const user = userEvent.setup();
+
+    await user.type(screen.getByLabelText(/First Name/i), "A");
+    await user.type(screen.getByLabelText(/Last Name/i), "B");
+    await user.click(screen.getByRole("button", { name: /submit/i }));
+
+    expect(screen.queryByText(/Invalid first name/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Invalid last name/i)).toBeInTheDocument();
+
+    await user.clear(screen.getByLabelText(/First Name/i));
+    await user.clear(screen.getByLabelText(/Last Name/i));
+
+
+    await user.type(screen.getByLabelText(/First Name/i), "A".repeat(51));
+    await user.type(screen.getByLabelText(/Last Name/i), "B".repeat(51));
     await user.click(screen.getByRole("button", { name: /submit/i }));
 
     expect(screen.queryByText(/Invalid first name/i)).toBeInTheDocument();
