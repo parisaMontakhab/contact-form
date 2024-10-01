ec46751bdbd7b18f4f0be5dde7f9c7b22408e99b
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-01 17:47:59 +0200

    test--feat: add MyMessage.test.jsx file

---
+++ tests/components/MyMessage.test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MyMessage from "../../src/components/MyMessage";
+import userEvent from "@testing-library/user-event";
+
+describe('MyMessage', () => {
+    
+})
