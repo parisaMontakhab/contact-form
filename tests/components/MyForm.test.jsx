8770f04027730b962ecebad82662e50041c0163f
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-10-14 11:51:51 +0200

    Deploy built files to GitHub Pages

--- tests/components/MyForm.test.jsx
+++ tests/components/MyForm.test.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { act } from "react";
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
